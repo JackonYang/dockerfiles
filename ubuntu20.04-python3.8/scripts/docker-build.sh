8786481ec7136e41f4a0604631447dc89ee45939
Jackon Yang <i@jackon.me>
2021-01-16 17:46:04 +0800

    use https instead of http in apt-get mirror

--- ubuntu20.04-python3.8/scripts/docker-build.sh
+++ ubuntu20.04-python3.8/scripts/docker-build.sh
@@ -1,18 +1,14 @@
-branch_name=`git rev-parse --abbrev-ref HEAD`
-image_version=`basename $branch_name`
+# branch_name=`git rev-parse --abbrev-ref HEAD`
+. image_info.ini
 
 MYSELF=`pwd`/$0
 MYDIR="${MYSELF%/*}"
 
 project_dir=`dirname ${MYDIR}`
 
-image_basename=jackon/$(basename $project_dir)
-image_name=${image_basename}:${image_version}
-latest_tag=${image_basename}:latest
+latest_tag=${image_name}:latest
 
-echo "image_name=${image_basename}" > image_info.ini
-echo "image_version=${image_version}" >> image_info.ini
 
-docker build -f ${project_dir}/Dockerfile -t ${image_name} ${project_dir}
+docker build -f ${project_dir}/Dockerfile -t ${image_name}:${image_version} ${project_dir}
 
-docker tag ${image_name} ${latest_tag}
+docker tag ${image_name}:${image_version} ${latest_tag}
